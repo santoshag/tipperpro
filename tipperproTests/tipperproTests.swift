c06fb4d795ae36496ca8b0b1d771d7c33887e3b7
Santosh Gogi <santoshag@gmail.com>
2016-03-09 10:05:39 -0800

    Initial Commit

---
+++ tipperproTests/tipperproTests.swift
@@ -0,0 +1,36 @@
+//
+//  tipperproTests.swift
+//  tipperproTests
+//
+//  Created by santosh ajith gogi on 3/9/16.
+//  Copyright © 2016 santosh ajith gogi. All rights reserved.
+//
+
+import XCTest
+@testable import tipperpro
+
+class tipperproTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
